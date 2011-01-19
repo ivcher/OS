a79f1f689a63119d06f8eb572ac4ac6b9e32adcc
ivche <iv_cher@e1.ru>
2011-01-19 22:06:52 +0500

    ne stal menyat errno)

--- hello/hello.c
+++ hello/hello.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 #include <errno.h>
-
 int	main()
-{	char* str = "Err in Hello, world";
-	errno = 0;
-	printf("Hello, world!\n");
-	if(errno!=0)
-	{	perror(str);
-		return 1;
-	}
-	system("pause");
-	return 0;
+{	
+    printf("Hello, world!\n");
+
+    return errno;	
 }
